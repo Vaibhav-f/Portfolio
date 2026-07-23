c83aab3e34297eef1f9e6c4334c10b13875d9148
Vaibhav-f <vaibhavkushwaha136@gmail.com>
2026-07-23 10:35:02 +0530

    Updated projects

--- src/Components/Section1/Project.jsx
+++ src/Components/Section1/Project.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import project from '../../assets/project.webp'
 import  portfolio from '../../assets/portfolio.webp'
 import  movie from '../../assets/movie.webp'
+import  mian from '../../assets/mian.png'
 const Project = () => {
  
   return (
@@ -16,15 +17,15 @@ const Project = () => {
         
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mt-10'>
       <div className='bg-mauve-950 rounded-2xl overflow-hidden  h-full md:h-90 lg:h-130 w-full md:w-80 lg:w-96 '>
-      <img className=' w-full h-56 object-cover object-top-left' src={portfolio} alt="bcg" />
+      <img className=' w-full h-56 object-cover object-top-left' src={mian} alt="bcg" />
 
-      <h1 className='text-emerald-300 text-2xl font-bold tracking-wider  px-3 py-3'>Portfolio</h1>
-      <p className=' px-3 py-3 text-white'>"Built entirely by me from scratch — no templates, no shortcuts. This portfolio uses React, Tailwind CSS, and React Hooks to present my work, skills, and growth as a fresher frontend developer."</p>
+      <h1 className='text-emerald-300 text-2xl font-bold tracking-wider  px-3 py-3'>Job Tracker</h1>
+      <p className=' px-3 py-3 text-white'>"A responsive job application tracking platform built with React, Vite, Tailwind CSS, and Recharts. Manage applications, and analyze job search performance with an interactive dashboard."</p>
       <br />
-     <div className='flex flex-wrap gap-3 px-3 pb-4'>
-       <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2 ' href="https://portfolio-omega-brown-37.vercel.app/"> Live Demo</a>
+     <div className='flex flex-wrap gap-3 px-3 pb-8'>
+       <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2 ' href="https://jobtracker-three-sigma.vercel.app/"> Live Demo</a>
       
-     <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2 ' href="https://github.com/Vaibhav-f/Portfolio"> Github Link</a>
+     <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2 ' href="https://github.com/Vaibhav-f/JobTracker"> Github Link</a>
       
      </div>
        
@@ -32,6 +33,7 @@ const Project = () => {
       
 {/*
 new Project
+
 */}
 
      
@@ -75,16 +77,16 @@ new Project
 new Project
 */}
       <div className='bg-mauve-950 rounded-2xl overflow-hidden  h-full md:h-90 lg:h-130 w-full md:w-80 lg:w-96'>
-      <img className=' w-full h-56 object-cover object-top-left' src={movie} alt="bcg" />
+      <img className=' w-full h-56 object-cover object-top-left' src={portfolio} alt="bcg" />
 
-      <h1 className='text-emerald-300 text-2xl font-bold tracking-wider  px-3 py-3'>SeorinPlay</h1>
-      <p className=' px-3 py-3 text-white'>"A modern movie discovery platform powered by the TMDB API. Browse trending, popular, and top-rated movies through a fast and responsive interface."</p>
+      <h1 className='text-emerald-300 text-2xl font-bold tracking-wider  px-3 py-3'>Portfolio</h1>
+      <p className=' px-3 py-3 text-white'>"Built entirely by me from scratch — no templates, no shortcuts. This portfolio uses React, Tailwind CSS, and React Hooks to present my work, skills, and growth as a fresher frontend developer."</p>
       <br />
       
-       <div className='flex flex-wrap gap-3 px-3 pb-4 py-5'>
-      <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2' href="https://playseorin-three.vercel.app/"> Live Demo</a>
+       <div className='flex flex-wrap gap-3 px-3 '>
+      <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2' href="https://portfolio-omega-brown-37.vercel.app/"> Live Demo</a>
       
-     <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2' href="https://github.com/Vaibhav-f/SeorinPLay"> Github Link</a>
+     <a className='bg-cyan-500 rounded-xl font-semibold px-4 py-2' href="https://github.com/Vaibhav-f/Portfolio"> Github Link</a>
       </div>
       </div>
 
